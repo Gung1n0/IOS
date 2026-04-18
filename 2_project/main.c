eaa63b8c6e7f22a4ebbbf63b109ba0a0defc3f04
Gung1n0 <gungino88@gmail.com>
2026-04-18 23:51:42 +0200

    modified:   main.c 	modified:   proj2.h 	modified:   tests

--- 2_project/main.c
+++ 2_project/main.c
@@ -1,62 +1,173 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "proj2.h"
+
+#define shared_mem "sh_mem"
+
 void create_file()
 {
-FILE *file;
-file=fopen("proj2.out", "w"); // "a" ked iba píšem
-fclose(file);
+    FILE *file;
+    file = fopen("./proj2.out", "w");
+    fclose(file);
 }
-void append_file(char *string)
+void append_file(char *string, int *counter, int last_write)
 {
-FILE *file;
-file=fopen("proj2.out", "a");
+    FILE *file;
+    file = fopen("proj2.out", "a");
     int i = 0;
-    i = fprintf(file,"%s", string);
-    if(i == EOF)
-    fprintf(stderr, "failed to write");
-fclose(file);
+    if (last_write)
+        i = fprintf(file, "%i: %s", *counter, string);
+    else
+        i = fprintf(file, "%i: %s\n", *counter, string);
+    *counter = *counter + 1;
+    if (i == EOF)
+        fprintf(stderr, "failed to write");
+    fclose(file);
 }
 
+mem *create_shmem()
+{
+    //                  memory      mode + flags(if exists return -1), R/W by owner
+    int memory = shm_open(shared_mem, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+    if (memory == -1)
+    {
+        shm_unlink(shared_mem);
+        memory = shm_open(shared_mem, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+        if (memory == -1)
+            return NULL;
+    }
+
+    ftruncate(memory, sizeof(mem));
+
+    mem *Mem = mmap(NULL, sizeof(mem), PROT_READ | PROT_WRITE, MAP_SHARED, memory, 0);
+    if (Mem == MAP_FAILED)
+        return NULL;
+    else
+        return Mem;
+}
+
+void init_mem(mem *shm)
+{
+    shm->counter = 1;
+    shm->waiting = 0;
+    shm->done = 0;
+    shm->done = false;
+}
+int check_values(int N, int V, int K, int TV, int TN, int O)
+{
+
+    if (!(V > 0 && V < 10))
+    {
+        fprintf(stderr, "wrong cart count\n");
+        return 1;
+    }
+    if (!(N > 0 && N < 10000))
+    {
+        fprintf(stderr, "wrong visitor count\n");
+        return 1;
+    }
+    if (!(K > 3 && K < 41))
+    {
+        fprintf(stderr, "wrong cart capacity\n");
+        return 1;
+    }
+    if (!(TV > -1 && TV < 1001))
+    {
+        fprintf(stderr, "wrong time\n");
+        return 1;
+    }
+    if (!(TN > -1 && TN < 1001))
+    {
+        fprintf(stderr, "wrong max time\n");
+        return 1;
+    }
+    if (!(O > 0 && O < 101))
+    {
+        fprintf(stderr, "wrong distance\n");
+        return 1;
+    }
+}
 int main(int argc, char const *argv[])
 {
-    int counter = 1;
-    if (argc < 7)
-    return 0;
-    
-    int V = atoi(argv[1]), N = atoi(argv[2]), K = atoi(argv[3]), TV=atoi(argv[4]), TN=atoi(argv[5]), O=atoi(argv[6]);
-    if (!(V> 0 && V <10))
+    if (!(argc == 7))
     {
-        fprintf(stderr,"zlý počet vozíkov\n");
-        return 0;
-    }
-    if (!(N> 0 &&  N<10000))
-    {
-        fprintf(stderr,"zlý počet navštevnikov\n");
-        return 0;
-    }
-    if (!(K> 3 && K <41))
-    {
-        fprintf(stderr,"zlá kapacita vozíku\n");
-        return 0;
-    }
-    if (!(TV> -1 && TV <1001))
-    {
-        fprintf(stderr,"zlá doba jízdy\n");
-        return 0;
-    }
-    if (!(TN> -1 &&  TN<1001))
-    {
-        fprintf(stderr,"zlá maximálna doba\n");
-        return 0;
-    }
-    if (!(O> 0 && O <101))
-    {
-        fprintf(stderr,"zlý odstup\n");
-        return 0;
+        fprintf(stderr, "too few arguments");
+        return 1;
     }
 
-    create_file();
+    int V = atoi(argv[1]), N = atoi(argv[2]), K = atoi(argv[3]), TV = atoi(argv[4]), TN = atoi(argv[5]), O = atoi(argv[6]);
+    // int V =0, N = 1, K = 4, TV = 1, TN = 1, O = 1;
 
+    int check = check_values(N, V, K, TV, TN, O);
+    if (check == 1)
+        return 1;
+    mem *shmem;
+    shmem = create_shmem();
+    if (shmem == NULL)
+    {
+        fprintf(stderr, "shared memory allocation failed");
+        return 1;
+    }
+    init_mem(shmem);
+
+    int idV = 0;
+    int idN = 0;
+    int id = fork();
+    if (id == 0) // original process
+    {
+        // vytvorenie vozíkov
+        for (int i = 0; i < V; i++)
+        {
+            int temp = fork();
+            if (temp == -1)
+                return 1;
+            else if (temp == 0)
+            {
+                idV = i + 1;
+                break;
+            }
+        }
+        if (idV != 0)
+        {
+            printf("process s idv %i\n", idV);
+        }
+
+        // vytvorenie navštevnikov
+        if (idV == 0 && id == 0)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                int temp = fork();
+                if (temp == -1)
+                    return 1;
+                else if (temp == 0)
+                {
+                    idN = i + 1;
+                    break;
+                }
+            }
+            if (idN != 0)
+            {
+                printf("process s idn %i\n", idN);
+            }
+        }
+    }
+    else if (id > 0)
+    {
+        // vytvorenie dispečera
+    }
+    else
+    {
+        fprintf(stderr, "fork failed");
+        return 1;
+    }
+
+    munmap(shmem, sizeof(mem));
+    shm_unlink(shared_mem);
     return 0;
 }
---
+++ 2_project/proj2.h
@@ -0,0 +1,11 @@
+#ifndef proj2_h
+#define proj2_h
+#include <stdbool.h>
+typedef struct {
+int counter;
+int waiting;
+int done;
+bool will_come;
+} mem;
+
+#endif
